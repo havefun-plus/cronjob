1207a57e47e9b7f7164db34f63a540d963e00f10
fucangyu <cangyufu@gmail.com>
2019-01-16 18:56:21 +0800

    change schedule method

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -1,4 +1,3 @@
-from sspider.core.spiders import BaseSpider
 from tests.helpers import ensure_redis_clear
 from tests.spiders.spiders import ForTestSpider
 
@@ -12,21 +11,4 @@ def test_periodic(mocker, scheduler, connection):
     schedule_func.called = False
 
     scheduler.periodic(schedule_func, ForTestSpider)
-    assert schedule_func.called
-
-
-def test_scheduler_all(mocker, scheduler):
-    from sspider.core.scheduler import Scheduler
-    ns = mocker.patch.object(BaseSpider, 'need_schedule', return_value=True)
-
-    def schedule_func(self, spider_cls):
-        if spider_cls.need_schedule():
-            self.queue.enqueue(spider_cls.registry_key())
-            schedule_func.called = True
-
-    schedule_func.called = False
-    ForTestSpider.canceled = False
-    mocker.patch.object(Scheduler, 'schedule', new=schedule_func)
-    scheduler.schedule_all()
-    assert ns.called
     assert schedule_func.called
